a6f7b43455978399539a7a1878267e1f554caa4b
Aristide <aristide.bamazi@imsp-uac.org>
2024-11-05 00:32:16 +0100

    Wip: Adding unit tests

--- main_test.go
+++ main_test.go
@@ -9,7 +9,20 @@ import (
 
 func TestFactoryMethod(t *testing.T) {
 	emp := Empty()
+
 	if emp.Lower.Value != math.Inf(+1) {
-		log.Fatal(fmt.Sprintf("name=%v actual=%v expecting=%v", "Empty set lower bound", emp.Lower.Value, math.Inf(+1)))
+		log.Fatal(fmt.Sprintf("name=%v actual=%v expecting=%v", "Empty set lower bound value", emp.Lower.Value, math.Inf(+1)))
+	}
+
+	if emp.Upper.Value != math.Inf(-1) {
+		log.Fatal(fmt.Sprintf("name=%v actual=%v expecting=%v", "Empty set upper bound value", emp.Upper.Value, math.Inf(-1)))
+	}
+
+	if emp.Lower.Type != LPAREN {
+		log.Fatal(fmt.Sprintf("name=%v actual=%v expecting=%v", "Empty set lower bound type", emp.Lower.Type, LPAREN))
+	}
+
+	if emp.Upper.Type != RPAREN {
+		log.Fatal(fmt.Sprintf("name=%v actual=%v expecting=%v", "Empty set upper bound type", emp.Upper.Type, RPAREN))
 	}
 }
